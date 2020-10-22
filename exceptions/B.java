90152fc18d85dbaff65c846331961a90b3d235b1
Niranjan B <niranjannb7777@gmail.com>
2020-10-22 19:35:42 +0530

    Lab 5

--- exceptions/B.java
+++ exceptions/B.java
@@ -4,24 +4,22 @@ import java.io.*;
 
 public class B {
     public static void main(String args[]) {
-        String c;
-        System.out.println("10"+c);
-//        try {
-//            String.txt = "new";
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        try {
-//            String str = args[2];
-//            int x = Integer.parseInt(args[0]);
-//            FileInputStream in = new FileInputStream("input.txt");
-//            System.out.println("This is not printed");
-//        } catch (IndexOutOfBoundsException e) {
-//            System.out.println("IndexOutOfBoundsException: caused");
-//        } catch (NumberFormatException e) {
-//            System.out.println("NumberFormatException: caused");
-//        } catch (FileNotFoundException e) {
-//            System.out.println("FileNotFoundException: caused");
-//        }
+        try {
+            String.txt = "new";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            String str = args[2];
+            int x = Integer.parseInt(args[0]);
+            FileInputStream in = new FileInputStream("input.txt");
+            System.out.println("This is not printed");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("IndexOutOfBoundsException: caused");
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException: caused");
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException: caused");
+        }
     }
 }
